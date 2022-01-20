1d12946bdfe36e1ae416dd0e4af0b308352f7d7c
Abhay Paidipalli <abhay.paidipalli@gmail.com>
2022-01-19 20:39:38 -0600

    initial setup

---
+++ SideScroller/SideScroller/SideScrollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideScrollerApp.swift
+//  SideScroller
+//
+//  Created by Abhay Paidipalli on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct SideScrollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
